0a4412a9428a916f987d119dc45d2e6e283e2d34
kaffikoda <16902308+kaffikoda@users.noreply.github.com>
2021-08-03 18:21:57 +0530

    generated random password with options like uppercase, special characters and numbers

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 # Create your views here.
@@ -9,4 +10,23 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+    lower_case_characters_list = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        lower_case_characters_list.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        lower_case_characters_list.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('number'):
+        lower_case_characters_list.extend(list('0123456789'))
+
+    length_of_password = int(request.GET.get('length', 12))
+    the_password = ''
+
+    # print(lower_case_characters_list)
+
+    for x in range(length_of_password):
+        the_password += random.choice(lower_case_characters_list)
+
+    return render(request, 'generator/password.html', {'password': the_password})
